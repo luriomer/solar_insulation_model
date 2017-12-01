d900bc9008d131041ec3f3b6e825439a94e22468
HEIHACHI\Omer <luriomer@gmail.com>
2017-12-02 01:57:22 +0200

    Changed the structure of the code - runs through several loops in order to save and record all data - including every hour at every day throughout the year. Also, plots the results in several ways + the empirical data gathered from Dor.

--- insulation_model.py
+++ insulation_model.py
@@ -17,67 +17,104 @@ import matplotlib.pyplot as plt
 
 
 ''' Las Vegas and universal parameters '''
-axial_tilt = 23.45*np.pi/180 # Earth axial tilt angle [rad]
+axial_tilt = 23.45*(np.pi/180) # Earth axial tilt angle [rad]
 L_std = 120 # Standart longtitude line (west) [deg]
 L = 115     # Actual longtitude line (west) [deg]
-phi = 36*np.pi/180 #Latitude line (north) [rad]
+phi = 36*(np.pi/180) #Latitude line (north) [rad]
 G0 = 1367 # Solar constant [W/m**2]
 A = 0.610 # Altitude [km]
 
+
 ''' Independent variables '''
-N = np.linspace(1,365,365) # Days throughout the year
-B = (2*np.pi*(N-1)/365)*np.pi/180 # Assisting parameter [rad]
-E = 229.2*(0.000075+0.001868*np.cos(B)-0.03208*np.sin(B)-0.01462*np.cos(2*B)-0.04089*np.sin(2*B))
+days = np.arange(1,366,1) # Days throughout the year
+hours = np.arange(7,18,1)
+B = (2*np.pi*(days-1)/365)*np.pi/180 # Assisting parameter [rad]
+
 
 ''' Calculation of declination angle '''
 delta = axial_tilt*np.sin((B-80*2*np.pi/365)*np.pi/180) #Declination angle [rad]
-#delta_t_solar = 4*(L_std-L)+E #Solar time difference (noon to noon) [minutes]
+
 
 ''' Calculation of Hottel correlation coefficients. 
     In Las Vegas, summer is 21 June to 22 September '''
-r0 = np.zeros_like(N)
-r1 = np.zeros_like(N)
-rk = np.zeros_like(N)
-for i in range(len(N)):
-    if (N[i]>=171 and N[i]<=262):
-        r0[i] = 1.03
-        r1[i] = 1.01
-        rk[i] = 1.00
-    else:
+r0 = np.zeros_like(days, dtype=float)
+r1 = np.zeros_like(days, dtype=float)
+rk = np.zeros_like(days, dtype=float)
+for i in range(len(days)):
+    if (days[i]>=171 and days[i]<=262):
         r0[i] = 0.97
         r1[i] = 0.99
         rk[i] = 1.02
+    else:
+        r0[i] = 1.03
+        r1[i] = 1.01
+        rk[i] = 1.00
 a0 = r0*(0.4237-0.00821*(6-A)**2)
 a1 = r1*(0.5055+0.00595*(6.5-A)**2)
 k = rk*(0.2711+0.01858*(2.5-A)**2)
 
-''' Calculation and plot of flux in different times during the day using the correlations'''
-Gan = zeros_like(N)
+
+''' Preperation of array variables '''
+shape = [len(days),len(hours)] # General array matrix shape [days and hours]
+cos_theta_z = np.zeros(shape, dtype=float)
+tau_b = np.zeros(shape, dtype=float)
+tau_d = np.zeros(shape, dtype=float)
+Gb = np.zeros(shape, dtype=float) # Beam flux (instant)
+Gd = np.zeros(shape, dtype=float) # Diffuse flux (instant)
+Gtot = np.zeros(shape, dtype=float) # Total flux (instant)
+Gav = np.zeros_like(days, dtype=float) # Daily total flux (average)
+
+
+''' Calculation of daily parameters '''
+delta_t_solar = hours-12
+omega = (delta_t_solar*360/(24))*np.pi/180 #Hour angle [rad]
+
+
+''' Calculation of flux in different times during the day using the correlations'''
+
+for i in range(len(days)):
+    for j in range(len(delta_t_solar)):
+        cos_theta_z[i,j] = (np.cos(phi)*np.cos(delta[i])*np.cos(omega[j])+np.sin(phi)*np.sin(delta[i]))
+        tau_b[i,j] = a0[i]+a1[i]*np.exp(-k[i]/cos_theta_z[i,j])
+        Gb[i,j] = G0*tau_b[i,j]
+        tau_d[i,j] = cos_theta_z[i,j]*(0.271-0.294*tau_b[i,j])
+        Gd[i,j] = G0*tau_d[i,j]
+        Gtot[i,j] = Gb[i,j] + Gd[i,j]
+    Gav[i] = np.average(Gtot[i])
+
+
+''' Importing empirical data '''
+Gav_emp = np.loadtxt('empirical_insulation.txt')
+
+
+''' Plotting the results '''
 plt.close('all')
-fig, ax1 = plt.subplots(2, sharex = True)
-fig, ax2 = plt.subplots()
-delta_ts = np.arange(-5,6,1)
-for delta_t_solar in delta_ts:
-    omega = delta_t_solar*360/(24*60) #Hour angle [deg]
-    cos_theta_z = (np.cos(phi)*np.cos(delta)*np.cos(omega)+np.sin(phi)*np.sin(delta))
-    AM = 1/cos_theta_z
-    tau_b = a0+a1*np.exp(-k*AM)
-    Gb = G0*tau_b
-    tau_d = cos_theta_z*(0.271-0.294*tau_b)
-    Gd = G0*tau_d
-    Gtot = Gb + Gd
-    ax1[0].plot(N,Gb, label = 'Solar noon difference = '+str(np.abs(delta_t_solar)))
-    ax1[1].plot(N,Gd, label = 'Solar noon difference = '+str(np.abs(delta_t_solar)))
-    ax2.plot(N,Gtot, label = 'Solar noon difference = '+str(np.abs(delta_t_solar)))
-#ax1[0].set_xlabel('Day number N (N=1 for 1 January)')
-ax1[0].set_ylabel('Beam insulation flux G [W/m**2]')
-ax1[0].legend(loc = 4)
-ax1[0].grid()
-ax1[1].set_xlabel('Day number N (N=1 for 1 January)')
-ax1[1].set_ylabel('Diffuse insulation flux G [W/m**2]')
-ax1[1].legend(loc = 4)
-ax1[1].grid()
-ax2.set_xlabel('Day number N (N=1 for 1 January)')
-ax2.set_ylabel('Total insulation flux G [W/m**2]')
-ax2.legend(loc = 4)
-ax2.grid()
+
+fig1 = plt.figure()
+
+ax1 = fig1.add_subplot(221)
+ax1.plot(days,Gav, label = "Simulation")
+ax1.plot(days,Gav_emp, label = "Empirical")
+ax1.set_xlabel("Day")
+ax1.set_ylabel("Average flux [W/m$^2$]")
+ax1.set_title("Average daily throught the year")
+ax1.legend()
+ax1.grid()
+
+ax2 = fig1.add_subplot(222)
+for i in range(len(delta_t_solar)):
+    ax2.plot(days,Gtot[:,i],label = "$\Delta$t = "+str(delta_t_solar[i]))
+    ax2.set_xlabel("Day")
+    ax2.set_ylabel("Total flux [W/m$^2$]")
+    ax2.set_title("Total daily flux throughout the year")
+    ax2.legend()
+    ax2.grid()
+
+ax3 = fig1.add_subplot(223)
+for j in range(len(days)):
+    ax3.plot(hours,Gtot[j])
+    ax3.set_xlabel("Hour")
+    ax3.set_ylabel("Total flux [W/m$^2$]")
+    ax3.set_title("Total flux throughout the day")
+    ax3.legend()
+    ax3.grid()
