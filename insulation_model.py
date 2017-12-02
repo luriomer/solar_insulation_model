Bug fix with negative cos_theta due to surface orientation. problem was solved using max([],0) inside loop calculation so there is no negative flux possible.

insulation_model.py
@@ -24,7 +24,7 @@ G0 = 1367 # Solar constant [W/m**2]
 A = 0.610 # Altitude [km]
 
 ''' Surface vector '''
-surf_p = np.array([0,0,2])
+surf_p = np.array([0,1,1])
 surf_p = surf_p/np.sqrt(np.dot(surf_p,surf_p)) #Normalization of surface vector
 
 ''' Independent variables '''
@@ -84,10 +84,10 @@ for i in range(len(days)):
         sun_vec[2,i,j] = np.cos(phi)*np.cos(delta[i])*np.cos(omega[j])+np.sin(phi)*np.sin(delta[i])
         cos_theta[i,j] = (np.dot(sun_vec[:,i,j],surf_p))/(np.linalg.norm(sun_vec[:,i,j])*np.linalg.norm(surf_p))
         #zenith[i,j] = (np.cos(phi)*np.cos(delta[i])*np.cos(omega[j])+np.sin(phi)*np.sin(delta[i]))
-        tau_b[i,j] = a0[i]+a1[i]*np.exp(-k[i]/cos_theta[i,j])
-        Gb[i,j] = G0*tau_b[i,j]*cos_theta[i,j]
-        tau_d[i,j] = cos_theta[i,j]*(0.271-0.294*tau_b[i,j])
-        Gd[i,j] = G0*tau_d[i,j]
+        tau_b[i,j] =max((a0[i]+a1[i]*np.exp(-k[i]/cos_theta[i,j])),0)
+        Gb[i,j] = max((G0*tau_b[i,j]*cos_theta[i,j]),0)
+        tau_d[i,j] = max((cos_theta[i,j]*(0.271-0.294*tau_b[i,j])),0)
+        Gd[i,j] = max((G0*tau_d[i,j]*cos_theta[i,j]),0)
         Gtot[i,j] = Gb[i,j] + Gd[i,j]
     Gav_day[i] = np.average(Gtot[i])
     Gb_av[i] = np.average(Gb[i])
@@ -105,7 +105,8 @@ E_emp = round(np.sum(Gav_emp)*24/1e6,2) # Multiply every day average by 12 hours
 plt.close('all')
 
 fig1 = plt.figure()
-fig1.suptitle("Simulation results for Las-Vegas, NV. Surface normal = "+str(surf_p), fontweight = "bold", fontsize=20)
+fig1.suptitle("Simulation results for Las-Vegas, NV.\nSurface normal = "
+              +str(surf_p), fontweight = "bold", fontsize=18)
 ax1 = fig1.add_subplot(221)
 ax1.plot(days,Gav_day, label = "Simulation")
 ax1.plot(days,Gav_emp*2, label = "Empirical") #Multiplting since the eimpirical averaging is for 24 hours
@@ -116,7 +117,8 @@ ax1.set_title("Average daily flux based on 12 hours of daylight")
 ax1.annotate("Total annual energy per unit area: \n "+
          "--------------------------------------------------\n"+
          "Theoretical (simulation) E = "+str(E_sim)+" [MWh/m$^2$] \n"+
-         "Empirical (atmospheric data) E = "+str(E_emp)+" [MWh/m$^2$]", fontsize = 11, color="red", fontweight = "bold", xy=(70, 240))
+         "Empirical (atmospheric data) E = "+str(E_emp)+" [MWh/m$^2$]", 
+         fontsize = 11, color="red", fontweight = "bold", xy=(70, 240))
 
 ax1.legend()
 ax1.grid()