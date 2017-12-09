Found the source of the bug - when the sun vector is perpendicular to the ground (sunrise and sunset), the tau_b calculation explodes (the exponent is the problem). Fixed by calculating only for positive zenith angle (so 0 at night is brutally given by a statement). Now we have to validate the results depending the surfce tilting angle. Seems like (-0.5,0,1) is the optimal normal (30 degrees to the south- like in Israel).

runner.py
@@ -26,7 +26,7 @@ phi = 36*(np.pi/180) #Latitude line (north) [rad]
 G0 = 1367 # Solar constant [W/m**2]
 A = 0.610 # Altitude [km]
 summer = [171,262] # Summer priod [start,end] [1 = Jan 1st]. In Las-Vegas June 21 - Sep 22
-surf_normal = surface_normal_calc(-1,0,1) #Change normal direction as needed
+surf_normal = surface_normal_calc(-0.5,0,1) #Change normal direction as needed. Looks like (-0.5,0,1) is optimal!
 empirical_data_path = 'empirical_insulation.txt'
 two_axis_tracking = False
 